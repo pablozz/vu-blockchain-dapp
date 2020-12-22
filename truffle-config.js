7efc6bf7c70816d379aedeb5605583c0250290c3
pawleq <57144337+pawleq@users.noreply.github.com>
2020-12-22 21:33:53 +0200

    added smart contract/migration

--- truffle-config.js
+++ truffle-config.js
@@ -1,15 +1,14 @@
 module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // for more about customizing your Truffle configuration!
   networks: {
-   development: {
-     host: "127.0.0.1",
-     port: 7545,
-     network_id: "*"
-   },
-   test: {
-     host: "127.0.0.1",
-     port: 7545,
-     network_id: "*"
-   }
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+    develop: {
+      port: 8545
+    }
   }
-  
 };
